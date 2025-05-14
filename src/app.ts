f14ea8632b1d8a359aed5f90f75c785cef6b3f88
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-14 07:58:42 -0300

    Refactor app.ts to standardize route imports and add CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'
 
-import authRoutes from './routes/authRoutes';
+import authRouter from './routes/Auth';
 import encryptRoutes from './routes/encryptRoutes';
 import decryptRoutes from './routes/decryptRoutes';
+import { autenticarToken } from './midddleware/Auth';
+import { errorHandler } from './midddleware/ErrorHandler';
 
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -26,9 +30,12 @@ mongoose
 // Middleware para interpretar JSON
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/encrypt', encryptRoutes);
-app.use('/decrypt', decryptRoutes);
+app.use('/auth', authRouter);
+app.use('/encrypt', autenticarToken, encryptRoutes);
+app.use('/decrypt', autenticarToken, decryptRoutes);
+
+
+app.use(errorHandler)
 
 // Inicia o servidor
 app.listen(PORT, () => {
